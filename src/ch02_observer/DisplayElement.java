6d9e3b30aff1b8b1c41989f24a91a58b6ac5191b
chori <slivers721@gmail.com>
2018-05-20 00:44:09 +0900

    observer pattern

---
+++ src/ch02_observer/DisplayElement.java
@@ -0,0 +1,5 @@
+package ch02_observer;
+
+public interface DisplayElement {
+    void display();
+}
---
+++ src/ch02_observer/Observer.java
@@ -0,0 +1,5 @@
+package ch02_observer;
+
+public interface Observer {
+    void update(float temperature, float humidity, float pressure);
+}
